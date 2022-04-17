d9457947779aaa8095151f30f42974ff6d8f4429
petersach2 <Peter.Sach1@diageo.com>
2022-04-17 06:12:09 +0100

    Add Get Databricks Scope Permissions

---
+++ Tests/Get-DatabricksSecretScopePermissions.tests.ps1
@@ -0,0 +1,24 @@
+param(
+    [ValidateSet('Bearer','ServicePrincipal')][string]$Mode="ServicePrincipal"
+)
+
+Set-Location $PSScriptRoot
+Import-Module "..\azure.databricks.cicd.tools.psd1" -Force
+$Config = (Get-Content '.\config.json' | ConvertFrom-Json)
+
+switch ($mode){
+    ("Bearer"){
+        Connect-Databricks -Region $Config.Region -BearerToken $Config.BearerToken
+    }
+    ("ServicePrincipal"){
+        Connect-Databricks -Region $Config.Region -DatabricksOrgId $Config.DatabricksOrgId -ApplicationId $Config.ApplicationId -Secret $Config.Secret -TenantId $Config.TenantId
+    }
+}
+
+Describe "Get-DatabricksSecretScopePermissions"{
+    
+    It "Get all permissions for scope"{
+        $json = Get-DatabricksSecretScopePermissions -BearerToken $BearerToken -Region $Region -ScopeName "Test1"
+    }
+
+}
